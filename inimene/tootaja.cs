7a2df0699a170f62c33c2e416c532c4349722c92
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-09-19 10:22:28 +0300

    Update tootaja.cs

--- inimene/tootaja.cs
+++ inimene/tootaja.cs
@@ -1,11 +1,23 @@
-﻿namespace OOPNaided.inimene;
+namespace OOPNaided.inimene;
 
 public class Töötaja : Inimene
 {
-    public string Ametikoht = "Politseinik";
+    public string Ametikoht = "Keevitaja";
+    public double Tunnitasu = 15.50;
+    public int Tunnid {  get; set; }
 
     public void Töötan()
     {
         Console.WriteLine($"{Nimi} töötab ametikohal {Ametikoht}.");
+    }
+
+    public override void Mida_teeb()
+    {
+        Console.WriteLine($"{Nimi} töötab ametikohal.");
+    }
+
+    public double ArvutaPalk()
+    {
+        return Tunnitasu * Tunnid;
     }
 }

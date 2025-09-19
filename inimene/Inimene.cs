7fa424b488c200473660f5ea0b6fbfe3de19ab2f
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-09-19 10:22:19 +0300

    Update Inimene.cs

--- inimene/Inimene.cs
+++ inimene/Inimene.cs
@@ -1,23 +1,28 @@
-﻿public class Inimene
+using OOPNaided.Pank;
+
+public abstract class Inimene
 {
     public string Nimi;
     public int Vanus;
+    public Pank Konto {  get; set; } 
 
     public Inimene()
     {
-
+        Konto = new Pank();
     }
 
     public Inimene(string nimi, int vanus)
     {
         Nimi = nimi;
         Vanus = vanus;
+        Konto = new Pank();
     }
 
     public void Tervita()
     {
         Console.WriteLine("Tere! Mina olen " + Nimi);
     }
+
     public abstract void Mida_teeb();
 }
 
